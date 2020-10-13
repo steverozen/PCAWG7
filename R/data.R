9d3a689f19c7db1ee47952d89283b4d8c88378d6
steverozen <steve@steverozen.net>
2020-10-13 15:49:17 +0800

    cleaned up documentation and new minor version number

--- R/data.R
+++ R/data.R
@@ -21,32 +21,134 @@
 #'
 #' @format
 #'
-#' A list with the following elements and sub-elements:
+#' A list with the elements:
 #'
 #' \describe{
-#'   \item{genome}{\describe{A list with the elements:
+#'   \item{genome}{A list with the elements:
+#'
+#'   \describe{
 #'     \item{SBS96}{Strand-agnostic single-base substitutions in trinucleotide context.}
 #'     \item{SBS192}{Transcriptionally stranded single-base substitutions in trinucleotide context.}
 #'     \item{DBS78}{Strand-agnostic doublet-base substitutions.}
 #'     \item{ID}{Strand-agnostic indels.}
 #'   }}
-#'   \item{exome}{\describe{A list with the elements:
-#'     \item{SBS96}{Strand-agnostic single-base substitutions in trinucleotide context.}
+#'
+#'   \item{exome}{A list with the elements:
+#'
+#'   \describe{
+#'     \item{SBS96}{As above, for exome count signatures, which look
+#'           different than genome count signatures, because of
+#'           differences in trinucleotide frequencies in exomes versus
+#'           whole genomes.}
 #'  }}
 #' }
 #'
-#' @name sgnature
+#' @name signature
 "signature"
 
-#' Vectors of PCAWG \code{tumor_wgs_icgc_specimin_id}.
+
+#' PCAWG7 SigProfiler assignments (exposures).
 #'
-#' Note the the PCAWG7 spectra catalogs have 2 blacklisted
-#' sample ids (they were blacklisted after the analysis was
-#' underway). The blacklisted samples are \code{SP116419}
-#' and \code{SP116883}.
+#' @source Files of \url{https://www.synapse.org/#!Synapse:syn12009743},
+#'   2019 Oct 09, populated by
+#'  \code{data-raw/sig.profiler.exposures/load.package.variable.exposure.R}.
+#'
+#' @format
+#'
+#' A list with the elements:
+#'
+#' \describe{
+#'   \item{PCAWG}{A list with the elements:
+#'   \describe{
+#'     \item{SBS96}{Strand-agnostic single-base substitutions
+#'                  in trinucleotide context.}
+#'     \item{DBS78}{Strand-agnostic doublet-base substitutions.}
+#'     \item{ID}{Strand-agnostic indels.}
+#'     These are signature assignments for the PCAWG platinum genomes.
+#'   }}
+#'   \item{TCGA}{A list with the elements:
+#'   \describe{
+#'     \item{SBS96}{As above.}
+#'     \item{ID}{As above.}
+#'     These are signature assignments for the TCGA exomes.
+#'   }}
+#'   \item{other.genome}{A list with the element:
+#'   \describe{
+#'      \item{SBS96}{As above.}
+#'      This contains signature assignments for non-TCGA
+#'      genomes.}}
+#'   \item{other.exome}{A list with the element:
+#'   \describe{
+#'      \item{SBS96}{As above.}
+#'      This contains signature assignments for non-TCGA
+#'      exomes.}}
+#' }
+#'
+#' @name exposure
+"exposure"
+
+########
+#' PCAWG7 mutational spectra (catalogs).
+#'
+#' @source Files below \url{https://www.synapse.org/#!Synapse:syn11801889},
+#'   2019 Oct 09. Populated by
+#'   \code{data-raw/spectra/load.package.variable.specra.R}.
+#'
+#' @format
+#'
+#' A list with the elements:
+#'
+#' \describe{
+#'   \item{SBS96}{Deprecated.}
+#'
+#'   \item{DBS78}{Deprecated.}
+#'
+#'   \item{PCAWG}{A list with the elements:
+#'   \describe{
+#'     \item{SBS96}{Strand-agnostic single-base substitutions
+#'                  in trinucleotide context.}
+#'     \item{SBS192}{Single-base substitutions in transcripts
+#'                  based on the sense strand.}
+#'     \item{SBS1536}{Strand-agnostic single-base substitutions
+#'                  in pentanucleotide context.}
+#'
+#'     \item{DBS78}{Strand-agnostic doublet-base substitutions.}
+#'     \item{ID}{Strand-agnostic indels.}
+#'
+#'   }}
+#'   \item{TCGA}{A list with the same elements as the
+#'               \code{PCAWG} element.}
+#'   \item{other.genome}{A list with the same elements as the
+#'               \code{PCAWG} element but with \code{ID} omitted.}
+#'   \item{other.exome}{A list with the same elements as the
+#'               \code{PCAWG} element but with \code{ID} omitted.}
+#' }
+#'
+#' @name spectra
+"spectra"
+
+
+#' Vectors of the PCAWG \code{tumor_wgs_icgc_specimin_id}s.
+#'
+#' Note that the PCAWG7 spectra catalogs have 2
+#' sample ids that were blacklisted after the mutational
+#' signature analysis was
+#' underway. The blacklisted samples are \code{SP116419}
+#' and \code{SP116883}, which are in \code{PCAWG.sample.id$black}.
 #'
 #' @source \url{https://dcc.icgc.org/api/v1/download?fn=/PCAWG/data_releases/latest/release_may2016.v1.4.with_consensus_calls.tsv},
 #'  2019 Oct 09
 #'
+#' @format
+#'
+#' A list with the elements:
+#'
+#' \describe{
+#'   \item{white}{Whitelisted IDs}
+#'   \item{grey}{Greylisted IDs}
+#'   \item{black}{Blacklisted IDs}
+#' }
+#'
+
 #' @name PCAWG.sample.id
 "PCAWG.sample.id"
--- news.md
+++ news.md
@@ -1,9 +1,12 @@
+# PCAWG7 0.0.2
+* Fully populated and documented exposure and spectra
+* All changes backward compatible
+
 # PCAWG7 0.0.0.9006
 * Corrected ID signatures; they were totally wrong.
 * Made sure abundances were present where possible in signatures.
 
-
-# PCAWG8 0.0.0.9004
+# PCAWG7 0.0.0.9004
 
 * Added indel (ID) signatures.
 * Removed BSgenome objects as reference genomes (causing problems in clients).
