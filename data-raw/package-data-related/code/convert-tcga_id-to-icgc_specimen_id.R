7d00de708318e40abf2d72807ae61a0f2a42a6d4
jnh01 <44010028+jnh01@users.noreply.github.com>
2021-03-23 19:28:21 +0800

    Updated code for generating internal data TCGA.ICGC.IDs

--- data-raw/package-data-related/code/convert-tcga_id-to-icgc_specimen_id.R
+++ data-raw/package-data-related/code/convert-tcga_id-to-icgc_specimen_id.R
@@ -4,18 +4,21 @@ cat(getwd(), "\n")
 
 tcga.uuid.file <- "data-raw/pc_annotation-tcga_uuid2barcode.tsv.gz"
 tcga.uuid.dt <- data.table::fread(tcga.uuid.file, header = FALSE)
-colnames(tcga.uuid.dt) <- c("cancer_type", "sample_info", "submitter_specimen_id", "tcga_id")
+
+# Only select those rows that have donor ID
+tcga.uuid.dt <- tcga.uuid.dt[V2 == "donor", ]
+colnames(tcga.uuid.dt) <- c("cancer_type", "sample_info", "submitter_donor_id", "tcga_id")
 
 pcawg.uuid.file <- "data-raw/pcawg_sample_sheet.v1.4.2016-09-14.tsv"
 pcawg.uuid.dt <- data.table::fread(pcawg.uuid.file)
 
-merged.dt <- dplyr::inner_join(pcawg.uuid.dt, tcga.uuid.dt, by = "submitter_specimen_id")
+merged.dt <- dplyr::inner_join(pcawg.uuid.dt, tcga.uuid.dt, by = "submitter_donor_id")
 print(object.size(merged.dt), units = "auto")
 
-# Only keep three columns "tcga_id", "submitter_specimen_id",
+# Only keep four columns "tcga_id", "submitter_donor_id",
 # "icgc_specimen_id", "dcc_specimen_type"
 needed.column.names <-
-  c("tcga_id", "submitter_specimen_id", "icgc_specimen_id", "dcc_specimen_type")
+  c("tcga_id", "submitter_donor_id", "icgc_specimen_id", "dcc_specimen_type")
 needed.dt <- merged.dt[, ..needed.column.names]
 
 # Keep only tumour samples
