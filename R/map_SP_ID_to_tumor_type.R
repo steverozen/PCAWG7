d4abda60422f809cc474dea8cfcef564e60ddc7a
Steven G. Rozen <steve@steverozen.net>
2021-08-08 11:16:04 +0800

    new function and some cleanup

--- R/map_SP_ID_to_tumor_type.R
+++ R/map_SP_ID_to_tumor_type.R
@@ -1,3 +1,17 @@
+#' Given PCAWG "SP" IDs (eg. SP123958) return either the "full" IDs (Kidney-ChRCC::SP123958) or just the tumor type (Kidney-ChRCC)
+#'
+#' @param SP.IDs A character vector of PCAWG "SP" IDs.
+#'
+#' @param merge If \code{TRUE} return a parallel vector of <tumor_type>::<SP_ID>; otherwise just <tumor_type>.
+#'
+#' @details Fails with an "subscript out of bounds" error if any of the elements of \code{SP.IDs} is unknown.
+#'
+#' @export
+#'
+#' @examples
+#' map_SP_ID_to_tumor_type(c("SP123958", "SP43633"))
+#' map_SP_ID_to_tumor_type(c("SP123958", "SP43633"), merge = FALSE)
+
 map_SP_ID_to_tumor_type <- function(SP.IDs, merge = TRUE) {
   cc <- colnames(PCAWG7::spectra$PCAWG$SBS96)
   zz <- strsplit(cc, "::", fixed = TRUE)
